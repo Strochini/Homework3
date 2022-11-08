27c8a4e8df42d812e17fd0762af5065e282f76c6
Strochini <0.618@bk.ru>
2022-11-09 01:49:56 +0300

    Frinds & Frog

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var friends = 19;
+        System.out.println("Friends " + friends);
+        friends = friends + 2;
+        System.out.println("Friends+ " + friends);
+        friends = friends / 7;
+        System.out.println("Friends- " + friends);
+
+        var frog = 3.5;
+        System.out.println("Frog " + frog);
+        frog = frog * 10;
+        System.out.println("Frog* " + frog);
+        frog = frog / 3.5;
+        System.out.println("Frog/ " + frog);
+        frog = frog + 4;
+        System.out.println("Frog+ " + frog);
     }
 }
